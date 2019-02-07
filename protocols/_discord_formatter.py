89aea04a154acce01a9c612d10ff4402081c7a1c
James Lu <james@overdrivenetworks.com>
2019-02-06 21:27:21 -0800

    GPL2 license text for discord_formatter

--- protocols/_discord_formatter.py
+++ protocols/_discord_formatter.py
@@ -1,3 +1,20 @@
+# From https://github.com/Hackndo/discord-irc-sync
+#
+# Copyright (C) 2017-2018 Hackndo <hackndo@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import re
 
 IRC_BOLD, IRC_ITALIC, IRC_UNDERLINE, IRC_RESET = ("\x02", "\x1d", "\x1f", "\x0f")
